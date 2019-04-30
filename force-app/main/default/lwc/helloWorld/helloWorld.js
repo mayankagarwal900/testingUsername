ef1581b2ac2a71e5d92e0563ab4e95402e4cd153
mayank <mayank.agarwal@briskminds.com>
2019-04-30 11:54:34 +0530

    new1 testing

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -1,6 +1,6 @@
 import { LightningElement, track } from 'lwc';
 export default class HelloWorld extends LightningElement {
-    @track greeting = 'World';
+    @track greeting = 'Wor';
     changeHandler(event) {
         this.greeting = event.target.value;
     }
